99b00e752570b40cf49f1749d1c2b294e9530bba
hansolpp <hansolmin3@gmail.com>
2020-05-22 18:13:46 +0900

    add resnet

--- Models.py
+++ Models.py
@@ -1,55 +1,35 @@
+from keras import Model
 from tensorflow.keras.layers import Dense, Conv2D, Dropout, Conv2DTranspose, MaxPooling2D, BatchNormalization, \
     Activation, concatenate, GlobalAveragePooling2D
 
 
-def build_model(input_layer, start_neurons):
-    # 40 x 40 -> 20 x 20
-    conv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(input_layer)
-    conv1 = BatchNormalization()(conv1)
-    conv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(conv1)
-    pool1 = BatchNormalization()(conv1)
-    pool1 = MaxPooling2D((2, 2))(pool1)
-    pool1 = Dropout(0.5)(pool1)
+def resnet_model(input_layer):
 
-    # 20 x 20 -> 10 x 10
-    conv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(pool1)
-    conv2 = BatchNormalization()(conv2)
-    conv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(conv2)
-    pool2 = BatchNormalization()(conv2)
-    pool2 = MaxPooling2D((2, 2))(pool2)
-    pool2 = Dropout(0.5)(pool2)
+    inputs = input_layer
 
-    # 10 x 10
-    convm = Conv2D(start_neurons * 4, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(pool2)
-    convm = BatchNormalization()(convm)
+    bn = BatchNormalization()(inputs)
+    conv0 = Conv2D(256, kernel_size=1, strides=1, padding='same', activation='relu')(bn)
 
-    # 10 x 10 -> 20 x 20
-    deconv2 = Conv2DTranspose(start_neurons * 2, (3, 3), strides=(2, 2), padding="same", kernel_initializer='he_normal')(convm)
-    deconv2 = BatchNormalization()(deconv2)
-    uconv2 = concatenate([deconv2, conv2])
-    uconv2 = BatchNormalization()(uconv2) #
-    uconv2 = Dropout(0.5)(uconv2)
-    uconv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(uconv2)
-    uconv2 = BatchNormalization()(uconv2)
-    uconv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(uconv2)
-    uconv2 = BatchNormalization()(uconv2)
+    bn = BatchNormalization()(conv0)
+    conv = Conv2D(128, kernel_size=2, strides=1, padding='same', activation='relu')(bn)
+    concat = concatenate([conv0, conv], axis=3)
 
-    # 20 x 20 -> 40 x 40
-    deconv1 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="same", kernel_initializer='he_normal')(uconv2)
-    uconv1 = concatenate([deconv1, conv1])
-    uconv1 = BatchNormalization()(uconv1) #
-    uconv1 = Dropout(0.5)(uconv1)
-    uconv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(uconv1)
-    uconv1 = BatchNormalization()(uconv1)
-    uconv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(uconv1)
-    uconv1 = BatchNormalization()(uconv1)
-    #uconv1 = Dropout(0.25)(uconv1)
-    output_layer = Conv2D(1, (1, 1), padding="same", activation='relu', kernel_initializer='he_normal')(uconv1)
+    bn = BatchNormalization()(concat)
+    conv = Conv2D(64, kernel_size=3, strides=1, padding='same', activation='relu')(bn)
+    concat = concatenate([concat, conv], axis=3)
 
-    return output_layer
+    for i in range(5):
+        bn = BatchNormalization()(concat)
+        conv = Conv2D(32, kernel_size=3, strides=1, padding='same', activation='relu')(bn)
+        concat = concatenate([concat, conv], axis=3)
+
+    bn = BatchNormalization()(concat)
+    outputs = Conv2D(1, kernel_size=1, strides=1, padding='same', activation='relu')(bn)
+
+    return outputs
 
 
-def my_model(input_layer, start_neurons):
+def unet_model(input_layer, start_neurons):
     # 40 x 40 -> 20 x 20
     conv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(input_layer)
     conv1 = BatchNormalization()(conv1)
